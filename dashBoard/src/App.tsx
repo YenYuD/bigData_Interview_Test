165001245e7dacdbd1177a3e45a8e25d6766b2e5
YenYuD <diaoemily0830@gmail.com>
2023-04-13 16:37:52 +0800

    修正img 高度

--- dashBoard/src/App.tsx
+++ dashBoard/src/App.tsx
@@ -7,8 +7,8 @@ import DashBoard from '../components/DashBoard'
 function App() {
 
   return (
-    <div className="App">
-      <img src={TaiwanSVG} className="fixed -z-10 top-[58px] max-tablet:opacity-[12%]"></img>
+    <div className="App ">
+      <img src={TaiwanSVG} className="fixed -z-10 top-[58px] h-[calc(100%-66px)] max-tablet:opacity-[12%] "></img>
       <Grid className="w-full px-4 py-[10px] bg-[#651FFF] flex justify-between h-[48px] items-center shadow-gray mb-[10px]">
         <p className="text-white  font-bold text-base font-ubuntu">LOGO</p>
         <Grid className="border border-white border-opacity-25 rounded-lg w-[30px] h-[30px] p-[7px] flex justify-center items-center">
